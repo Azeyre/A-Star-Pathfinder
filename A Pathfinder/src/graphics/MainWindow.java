84e233d0951369d8e7536ea47b3832d9b78e7bea
Azeyre <31286257+Azeyre@users.noreply.github.com>
2019-04-20 15:39:04 +0200

    Up

--- A Pathfinder/src/graphics/MainWindow.java
+++ A Pathfinder/src/graphics/MainWindow.java
@@ -3,6 +3,7 @@ package graphics;
 import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
@@ -28,7 +29,6 @@ public class MainWindow extends Application {
 	static Plateau p;
 	boolean setStart, setEnd;
 	Point current;
-	static int currentIndex;
 	static ArrayList<Point> road;
 	static ArrayList<Point> tested;
 	static boolean alreadyCheck[][];
@@ -39,7 +39,6 @@ public class MainWindow extends Application {
 		alreadyCheck = new boolean[size][size];
 		road = new ArrayList<Point>();
 		tested = new ArrayList<Point>();
-		currentIndex = 0;
 		p = new Plateau(size);
 		Application.launch();
 	}
@@ -49,9 +48,7 @@ public class MainWindow extends Application {
 		VBox root = new VBox();
 		Canvas canvas = new Canvas(taille_case * size, taille_case * size);
 		gc = canvas.getGraphicsContext2D();
-		repaint();
-		root.getChildren().add(canvas);
-		root.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
@@ -59,18 +56,36 @@ public class MainWindow extends Application {
 				if (setStart) {
 					p.setStart(x, y);
 					current = p.getStart();
+					current.setGCost(0);
 					road.add(current);
 					setStart = false;
 				} else if (setEnd) {
 					p.setEnd(x, y);
 					setEnd = false;
 				} else {
-					if (p.getPoint(x, y) != p.getStart() && p.getPoint(x, y) != p.getEnd() && !murs.contains(p.getPoint(x, y))) {
+					if (p.getPoint(x, y) != p.getStart() && p.getPoint(x, y) != p.getEnd()
+							&& !murs.contains(p.getPoint(x, y))) {
 						murs.add(p.getPoint(x, y));
 						System.out.println("Ajout d'un mur en [" + x + ";" + y + "]");
-					} else System.err.println("Impossible d'ajouter ce mur !");
+					} else
+						System.err.println("Impossible d'ajouter ce mur !");
 				}
 				repaint();
+			}
+
+		});
+		repaint();
+		root.getChildren().add(canvas);
+		root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				int x = (int) (event.getX() / taille_case), y = (int) (event.getY() / taille_case);
+				if (p.getPoint(x, y) != p.getStart() && p.getPoint(x, y) != p.getEnd()
+						&& !murs.contains(p.getPoint(x, y))) {
+					murs.add(p.getPoint(x, y));
+					System.out.println("Ajout d'un mur en [" + x + ";" + y + "]");
+				}
 			}
 
 		});
@@ -81,15 +96,15 @@ public class MainWindow extends Application {
 		Button end_point = new Button("End point");
 		Button search = new Button("Search Path");
 		Button next = new Button("Next");
+		Button clear = new Button("Clear");
 		search.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				if(p.getStart() != null && p.getEnd() != null) {
+				if (p.getStart() != null && p.getEnd() != null) {
 					current = p.getStart();
-					while(!finish) {
-						doNext();
-					}
-				} else System.err.println("Start or end isn't define !");
+					while (!doNext()) {}
+				} else
+					System.err.println("Start or end isn't define !");
 			}
 		});
 		start_point.setOnAction(new EventHandler<ActionEvent>() {
@@ -107,14 +122,30 @@ public class MainWindow extends Application {
 		next.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				if(finish) System.out.println("Trouvé");
-				else if(p.getStart() != null && p.getEnd() != null) {
+				if (finish)
+					System.out.println("Trouvé");
+				else if (p.getStart() != null && p.getEnd() != null) {
 					doNext();
-				} else System.err.println("Start or end isn't define !");
+				} else
+					System.err.println("Start or end isn't define !");
+			}
+		});
+		clear.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				murs = new ArrayList<Point>();
+				boolean setStart, setEnd;
+				current = null;
+				p.clear();
+				alreadyCheck = new boolean[size][size];
+				road = new ArrayList<Point>();
+				tested = new ArrayList<Point>();
+				finish = false;
+				repaint();
 			}
 		});
 
-		bottom.getChildren().addAll(start_point, end_point, search, next);
+		bottom.getChildren().addAll(start_point, end_point, search, next, clear);
 		root.getChildren().add(bottom);
 
 		Scene scene = new Scene(root);
@@ -132,25 +163,40 @@ public class MainWindow extends Application {
 				gc.strokeRect(i * taille_case, j * taille_case, taille_case, taille_case);
 			}
 		}
+		
 		gc.setFill(Color.BLACK);
 		for (int i = 0; i < murs.size(); i++) {
 			gc.fillRect(murs.get(i).getX() * taille_case, murs.get(i).getY() * taille_case, taille_case, taille_case);
 		}
+		
 		gc.setFill(Color.RED);
 		if (p.getStart() != null)
 			gc.fillRect(p.getStart().getX() * taille_case, p.getStart().getY() * taille_case, taille_case, taille_case);
+		
 		gc.setFill(Color.GREEN);
 		if (p.getEnd() != null)
 			gc.fillRect(p.getEnd().getX() * taille_case, p.getEnd().getY() * taille_case, taille_case, taille_case);
+		
 		gc.setFill(Color.BLUE);
-		for(int i = 0 ; i < road.size() ; i++) {
+		for (int i = 0; i < road.size(); i++) {
 			gc.fillRect(road.get(i).getX() * taille_case, road.get(i).getY() * taille_case, taille_case, taille_case);
 		}
 		
 		gc.setStroke(Color.BLACK);
-		for(int i = 0 ; i < size ; i++) {
-			for(int j = 0 ; j < size ; j++) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				afficheStats(p.getPoint(i, j));
+			}
+		}
+		
+		if(current != null) {
+			//gc.setFill(Color.rgb((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256)));
+			gc.setFill(Color.PURPLE);
+			Point temp = current;
+			while(temp.getPrecedent() != null) {
+				gc.fillRect(temp.getX() * taille_case, temp.getY() * taille_case, taille_case, taille_case);
+				afficheStats(temp);
+				temp = temp.getPrecedent();
 			}
 		}
 	}
@@ -167,65 +213,87 @@ public class MainWindow extends Application {
 			node.setScaleY(node.getScaleY() * zoomFactor);
 		});
 	}
-	
+
 	private void afficheStats(Point p) {
-		String s = String.valueOf(p.getGCost());
-		gc.strokeText(s, p.getX() * taille_case + 2, p.getY() * taille_case + 10);
+		String s;
+		if(p.getGCost() != 0) {
+			s = String.valueOf(p.getGCost());
+			gc.strokeText(s, p.getX() * taille_case + 2, p.getY() * taille_case + 10);
+		}
+
+		if(p.getHCost() != 0) {
+			s = String.valueOf(p.getHCost());
+			gc.strokeText(s, p.getX() * taille_case + 25, p.getY() * taille_case + 10);
+		}
 		
-		s = String.valueOf(p.getHCost());
-		gc.strokeText(s, p.getX() * taille_case + 25, p.getY() * taille_case + 10);
-		
-		s = String.valueOf(p.getFCost());
-		gc.strokeText(s, p.getX() * taille_case + 13, p.getY() * taille_case + 25);
+		if(p.getFCost() != 0) {
+			s = String.valueOf(p.getFCost());
+			gc.strokeText(s, p.getX() * taille_case + 13, p.getY() * taille_case + 25);
+		}
 	}
-	
-	private void doNext() {
-		Point temp = null;
-		Point best = null;
-		//System.out.println("Do");
-		currentIndex++;
-		gc.fillRect(current.getX() * taille_case, current.getY() * taille_case, taille_case, taille_case);
-		for(int i = -1 ; i <= 1 ; i++) {
-			for(int j = -1 ; j <= 1 ; j++) {
-				if(!(i == 0 && j == 0)) {
-					temp = null;
-					if(murs.contains(p.getPoint(current.getX() + i, current.getY() + j))) System.out.println("Mur");
-					else if(!road.contains(p.getPoint(current.getX() + i, current.getY() + j))){
-						temp = p.getPoint(current.getX() + i, current.getY() + j);
-					}					
-					if(temp != null && !alreadyCheck[current.getX() + i][current.getY() + j] && !tested.contains(temp)) {
-						//System.out.println("Temp");
-						int fromStart, toEnd;
-						fromStart = temp.getDistance(p.getStart());
-						toEnd = temp.getDistance(p.getEnd());
-						temp.setGCost(fromStart);
-						temp.setHCost(toEnd);
-						temp.setFCost(fromStart + toEnd);
-						alreadyCheck[current.getX() + i][current.getY() + j] = true;
-						tested.add(temp);
-						if(best == null) {
-							//System.out.println("BEST NULL : replace by : " + temp.toString());
-							best = temp;
-						} 
+
+	private boolean doNext() {
+		Point temp;
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (!(i == 0 && j == 0)) {
+					temp = p.getPoint(current.getX() + i, current.getY() + j);
+					if(temp != null && temp != p.getStart()) {
+						if(!murs.contains(temp)) { //Temp n'est pas un mur
+							if(temp.getPrecedent() == null) { //Les stats n'ont pas été initialisées
+								temp.setPrecedent(current);
+								int fromStart, toEnd;
+								fromStart = temp.getDistance(current) + current.getGCost();
+								toEnd = temp.getDistance(p.getEnd());
+								temp.setGCost(fromStart);
+								temp.setHCost(toEnd);
+								temp.setFCost(fromStart + toEnd);
+								tested.add(temp);
+								System.out.println("Point ajouté");
+							} else {
+								Point p2 = temp;
+								int fromStart, toEnd;
+								fromStart = temp.getDistance(current) + current.getGCost();
+								toEnd = temp.getDistance(p.getEnd());
+								if(fromStart < temp.getGCost()) {
+									temp.setGCost(fromStart);
+									temp.setHCost(toEnd);
+									temp.setFCost(fromStart + toEnd);
+									System.out.println("Point MODIFIE!");
+								}
+							}
+						}
 					}
 				}
 			}
 		}
 		current = getBest();
-		//System.out.println("LE NOUVEAU CURRENT : " + current.toString());
 		road.add(current);
-		if(current == null) System.out.println("CURRENT NULL" + "\n");
-		else if(current.getHCost() == 0) finish = true;
+		if(current == null) {
+			System.out.println("Aucun passage possible");
+			finish = true;
+			return true;
+		}
+		if (current.getHCost() == 0) {
+			finish = true;
+			System.out.println("Terminé");
+			repaint();
+			return true;
+		}
 		repaint();
+		return false;
 	}
-	
+
 	private Point getBest() {
 		Point temp = null;
-		for(int i = 0 ; i < tested.size() ; i++) {
-			if(!road.contains(tested.get(i))) {
-				if(temp == null) temp = tested.get(i);
-				if(tested.get(i).getFCost() < temp.getFCost()) temp = tested.get(i);
-				else if(tested.get(i).getFCost() == temp.getFCost() && tested.get(i).getHCost() < temp.getHCost()) temp = tested.get(i);
+		for (int i = 0; i < tested.size(); i++) {
+			if (!road.contains(tested.get(i))) {
+				if (temp == null)
+					temp = tested.get(i);
+				if (tested.get(i).getFCost() < temp.getFCost())
+					temp = tested.get(i);
+				else if (tested.get(i).getFCost() == temp.getFCost() && tested.get(i).getHCost() < temp.getHCost())
+					temp = tested.get(i);
 			}
 		}
 		return temp;
